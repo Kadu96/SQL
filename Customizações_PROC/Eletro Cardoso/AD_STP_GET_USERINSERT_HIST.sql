08ccc03c684f79b235582ab9d15722561f0b1ca3
Carlos Eduardo <cadusilva96@gmail.com>
2024-06-12 16:47:53 -0300

    .

---
+++ Customizações_PROC/Eletro Cardoso/AD_STP_GET_USERINSERT_HIST.sql
@@ -0,0 +1,65 @@
+CREATE OR REPLACE PROCEDURE "AD_STP_GET_USERINSERT_HIST" (
+       P_TIPOEVENTO INT,    -- Identifica o tipo de evento
+       P_IDSESSAO VARCHAR2, -- Identificador da execução. Serve para buscar informações dos campos da execução.
+       P_CODUSU INT         -- Código do usuário logado
+) AS
+       BEFORE_INSERT INT;
+       AFTER_INSERT  INT;
+       BEFORE_DELETE INT;
+       AFTER_DELETE  INT;
+       BEFORE_UPDATE INT;
+       AFTER_UPDATE  INT;
+       BEFORE_COMMIT INT;
+
+        P_CODPARC INT;
+        P_ID INT;
+
+BEGIN
+       BEFORE_INSERT := 0;
+       AFTER_INSERT  := 1;
+       BEFORE_DELETE := 2;
+       AFTER_DELETE  := 3;
+       BEFORE_UPDATE := 4;
+       AFTER_UPDATE  := 5;
+       BEFORE_COMMIT := 10;
+       
+/*******************************************************************************
+   É possível obter o valor dos campos através das Functions:
+   
+  EVP_GET_CAMPO_DTA(P_IDSESSAO, 'NOMECAMPO') -- PARA CAMPOS DE DATA
+  EVP_GET_CAMPO_INT(P_IDSESSAO, 'NOMECAMPO') -- PARA CAMPOS NUMÉRICOS INTEIROS
+  EVP_GET_CAMPO_DEC(P_IDSESSAO, 'NOMECAMPO') -- PARA CAMPOS NUMÉRICOS DECIMAIS
+  EVP_GET_CAMPO_TEXTO(P_IDSESSAO, 'NOMECAMPO')   -- PARA CAMPOS TEXTO
+  
+  O primeiro argumento é uma chave para esta execução. O segundo é o nome do campo.
+  
+  Para os eventos BEFORE UPDATE, BEFORE INSERT e AFTER DELETE todos os campos estarão disponíveis.
+  Para os demais, somente os campos que pertencem à PK
+  
+  * Os campos CLOB/TEXT serão enviados convertidos para VARCHAR(4000)
+  
+  Também é possível alterar o valor de um campo através das Stored procedures:
+  
+  EVP_SET_CAMPO_DTA(P_IDSESSAO,  'NOMECAMPO', VALOR) -- VALOR DEVE SER UMA DATA
+  EVP_SET_CAMPO_INT(P_IDSESSAO,  'NOMECAMPO', VALOR) -- VALOR DEVE SER UM NÚMERO INTEIRO
+  EVP_SET_CAMPO_DEC(P_IDSESSAO,  'NOMECAMPO', VALOR) -- VALOR DEVE SER UM NÚMERO DECIMAL
+  EVP_SET_CAMPO_TEXTO(P_IDSESSAO,  'NOMECAMPO', VALOR) -- VALOR DEVE SER UM TEXTO
+********************************************************************************/
+
+    IF P_TIPOEVENTO = AFTER_INSERT THEN
+        P_CODPARC := EVP_GET_CAMPO_INT(P_IDSESSAO, 'CODPARC');
+        P_ID := EVP_GET_CAMPO_INT(P_IDSESSAO, 'ID');
+
+        UPDATE AD_TGFPARH SET CODUSU = P_CODUSU, CODUSUALT = P_CODUSU WHERE CODPARC = P_CODPARC AND ID = P_ID;
+
+    END IF;
+
+    IF P_TIPOEVENTO = AFTER_UPDATE THEN
+        P_CODPARC := EVP_GET_CAMPO_INT(P_IDSESSAO, 'CODPARC');
+        P_ID := EVP_GET_CAMPO_INT(P_IDSESSAO, 'ID');
+
+        UPDATE AD_TGFPARH SET CODUSUALT = P_CODUSU WHERE CODPARC = P_CODPARC AND ID = P_ID;
+
+    END IF;
+
+END;
--- Customizações_PROC/Eletro Cardoso/AD_STP_UPD_TABPRECO2.sql
+++ Customizações_PROC/Eletro Cardoso/AD_STP_UPD_TABPRECO2.sql
@@ -5,6 +5,12 @@ CREATE OR REPLACE PROCEDURE "AD_STP_UPD_TABPRECO2" (
     P_MENSAGEM OUT VARCHAR2 -- Caso seja passada uma mensagem aqui, ela será exibida como uma informação ao usuário.
 ) AS
     FIELD_CODPROD NUMBER;
+    C_TAB INT := 0;
+    V_CODPROD NUMBER;
+    V_GRUPOICMS NUMBER;
+    V_GRUPOICMS2 NUMBER;
+    V_NCM NUMBER;
+    V_IDALIQ NUMBER;
 
 BEGIN
 
@@ -29,17 +35,17 @@ BEGIN
             PRO.ATIVO = 'S'
     )
     SELECT
-    P2.SEQUENCIA,
-    P2.CODPROD,
-    P2.GRUPOICMS,
-    P2.GRUPOICMS2,
-    P2.NCM,
-    P2.TOP,
-    P2.VLRVENDA AS VLRUNIT,
-    PR.VLRVENDA,
-    (CASE WHEN P.TEMIPIVENDA = 'S' THEN P2.VLRVENDA ELSE 0 END) AS BASEIPI,
-    (CASE WHEN P.TEMIPIVENDA = 'S' THEN NVL(I.PERCENTUAL,0) ELSE 0 END) AS PERCIPI,
-    (CASE WHEN P.TEMIPIVENDA = 'S' THEN (P2.VLRVENDA * I.PERCENTUAL / 100) ELSE 0 END) AS VLRIPI
+        P2.SEQUENCIA,
+        P2.CODPROD,
+        P2.GRUPOICMS,
+        P2.GRUPOICMS2,
+        P2.NCM,
+        P2.TOP,
+        P2.VLRVENDA AS VLRUNIT,
+        PR.VLRVENDA,
+        (CASE WHEN P.TEMIPIVENDA = 'S' THEN P2.VLRVENDA ELSE 0 END) AS BASEIPI,
+        (CASE WHEN P.TEMIPIVENDA = 'S' THEN NVL(I.PERCENTUAL,0) ELSE 0 END) AS PERCIPI,
+        (CASE WHEN P.TEMIPIVENDA = 'S' THEN (P2.VLRVENDA * I.PERCENTUAL / 100) ELSE 0 END) AS VLRIPI
     FROM TAB_P2 P2
     INNER JOIN TGFPRO P ON P.CODPROD = P2.CODPROD
     LEFT JOIN AD_TGFTABIPC PR ON PR.CODPROD = P.CODPROD
@@ -47,36 +53,87 @@ BEGIN
 
     /*ATUALIZA OS PRODUTOS NA TABELA AD_TGFTABBASE COM O IDALIQICMS*/
 
-    UPDATE AD_TGFTABBASE T
-    SET 
-    T.IDALIQICMS = (
-            WITH TAB_ALIQ AS (
-            SELECT
-                    I.IDALIQ, 
-                    I.CODTRIB,
-                    I.ALIQUOTA,
-                    I.ALIQSUBTRIB,
-                    I.TIPRESTRICAO, 
-                    I.TIPRESTRICAO2, 
-                    I.CODRESTRICAO, 
-                    I.CODRESTRICAO2,
-                    P.PRIORIDADE
-            FROM TGFICM I, TGFPRI P
-            WHERE I.UFORIG = 8
-                    AND I.UFDEST = 8
-                    AND I.TIPRESTRICAO = P.TIPRESTRICAO1
-                    AND I.TIPRESTRICAO2 = P.TIPRESTRICAO2
-            ) 
-            SELECT IDALIQ FROM TAB_ALIQ 
-            WHERE 
-            (PRIORIDADE = 560 AND CODRESTRICAO = T.GRUPOICMS AND CODRESTRICAO2 = T.TOP) OR
-            (PRIORIDADE = 563 AND CODRESTRICAO = T.NCM AND CODRESTRICAO2 = T.GRUPOICMS) OR
-            (PRIORIDADE = 564 AND CODRESTRICAO = T.TOP AND CODRESTRICAO2 = -1) OR
-            (PRIORIDADE = 565 AND CODRESTRICAO = NVL(T.GRUPOICMS2,'') AND CODRESTRICAO2 = T.NCM) OR
-            (PRIORIDADE = 566 AND CODRESTRICAO = NVL(T.GRUPOICMS2,'') AND CODRESTRICAO2 = -1) OR
-            (PRIORIDADE = 567 AND CODRESTRICAO = -1 AND CODRESTRICAO2 = -1) OR
-            (PRIORIDADE = 568 AND CODRESTRICAO = T.TOP AND CODRESTRICAO2 = 1))
-    WHERE T.CODPROD > 0;
+    SELECT COUNT(*) INTO C_TAB FROM AD_TGFTABBASE;
+
+    FOR I IN 1..C_TAB
+    LOOP
+        V_CODPROD := ACT_INT_FIELD(P_IDSESSAO, I, 'CODPROD');
+        /*SELECT NVL(GRUPOICMS,'') INTO V_GRUPOICMS FROM AD_TGFTABBASE WHERE CODPROD = FIELD_CODPROD;
+        SELECT NVL(GRUPOICMS2,'') INTO V_GRUPOICMS2 FROM AD_TGFTABBASE WHERE CODPROD = FIELD_CODPROD;
+        SELECT NVL(NCM,'') INTO V_NCM FROM AD_TGFTABBASE WHERE CODPROD = FIELD_CODPROD;*/
+        
+        UPDATE AD_TGFTABBASE T
+            SET T.ALIQPRIO = (
+                WITH TAB_ALIQ AS (
+                    SELECT
+                        I.IDALIQ, 
+                        I.CODTRIB,
+                        I.ALIQUOTA,
+                        I.ALIQSUBTRIB,
+                        I.TIPRESTRICAO, 
+                        I.TIPRESTRICAO2, 
+                        I.CODRESTRICAO, 
+                        I.CODRESTRICAO2,
+                        P.PRIORIDADE
+                    FROM TGFICM I
+                        INNER JOIN TGFPRI P ON I.TIPRESTRICAO = P.TIPRESTRICAO1 AND I.TIPRESTRICAO2 = P.TIPRESTRICAO2 
+                    WHERE I.UFORIG = 8 AND I.UFDEST = 8
+                ) 
+                SELECT MIN(PRIORIDADE) FROM TAB_ALIQ I
+                WHERE 
+                    (I.CODRESTRICAO = T.GRUPOICMS AND I.CODRESTRICAO2 = 1757) OR
+                    (I.CODRESTRICAO = T.NCM AND I.CODRESTRICAO2 = T.GRUPOICMS) OR
+                    (I.CODRESTRICAO = 1757 AND I.CODRESTRICAO2 = -1) OR
+                    (I.CODRESTRICAO = NVL(T.GRUPOICMS2,'') AND I.CODRESTRICAO2 = T.NCM) OR
+                    (I.CODRESTRICAO = NVL(T.GRUPOICMS2,'') AND I.CODRESTRICAO2 = -1) OR
+                    (I.CODRESTRICAO = 1757 AND I.CODRESTRICAO2 = 2) OR
+                    (I.CODRESTRICAO = -1 AND I.CODRESTRICAO2 = -1))
+        WHERE T.CODPROD = V_CODPROD;
+
+        UPDATE AD_TGFTABBASE T
+            SET T.IDALIQICMS = (
+                WITH TAB_ALIQ AS (
+                    SELECT
+                            I.IDALIQ, 
+                            I.CODTRIB,
+                            I.ALIQUOTA,
+                            I.ALIQSUBTRIB,
+                            I.TIPRESTRICAO, 
+                            I.TIPRESTRICAO2, 
+                            I.CODRESTRICAO, 
+                            I.CODRESTRICAO2,    
+                            P.PRIORIDADE
+                    FROM TGFICM I, TGFPRI P
+                    WHERE I.UFORIG = 8 AND I.UFDEST = 8
+                        AND I.TIPRESTRICAO = P.TIPRESTRICAO1
+                        AND I.TIPRESTRICAO2 = P.TIPRESTRICAO2
+                ) 
+                SELECT IDALIQ FROM TAB_ALIQ 
+                WHERE 
+                    CODRESTRICAO = 
+                        (CASE 
+                            WHEN T.ALIQPRIO = 604 THEN TO_NUMBER(T.GRUPOICMS)
+                            WHEN T.ALIQPRIO = 607 THEN TO_NUMBER(T.NCM)
+                            WHEN T.ALIQPRIO = 608 THEN 1757
+                            WHEN T.ALIQPRIO = 609 THEN NVL(TO_NUMBER(T.GRUPOICMS2),0)
+                            WHEN T.ALIQPRIO = 610 THEN NVL(TO_NUMBER(T.GRUPOICMS2),0)
+                            WHEN T.ALIQPRIO = 611 THEN 1757 
+                            WHEN T.ALIQPRIO = 612 THEN -1
+                        END) AND
+                    CODRESTRICAO2 = 
+                        (CASE 
+                            WHEN T.ALIQPRIO = 604 THEN 1757
+                            WHEN T.ALIQPRIO = 607 THEN TO_NUMBER(T.GRUPOICMS)
+                            WHEN T.ALIQPRIO = 608 THEN -1
+                            WHEN T.ALIQPRIO = 609 THEN TO_NUMBER(T.NCM)
+                            WHEN T.ALIQPRIO = 610 THEN -1
+                            WHEN T.ALIQPRIO = 611 THEN 2 
+                            WHEN T.ALIQPRIO = 612 THEN -1
+                        END)
+            )
+        WHERE T.CODPROD = V_CODPROD;
+
+    END LOOP;
 
     /*ATUALIZA A TABELA AD_TGFTABBASE COM OS VALORES DE BASESUBST, VLRSUBST, MVA E VLRBASE*/
 
@@ -86,52 +143,48 @@ BEGIN
             SELECT 
                 (CASE WHEN ICM.ALIQSUBTRIB > 0 THEN (
                     (T.VLRVENDA + 
-                        (CASE WHEN P.TEMIPIVENDA = 'S' THEN (T.VLRVENDA * I.PERCENTUAL / 100) ELSE 0 END)) * (1 + ICM.MARGLUCRO/100)) 
+                        (CASE WHEN P.TEMIPIVENDA = 'S' THEN (T.VLRVENDA * I.PERCENTUAL / 100) ELSE 0 END)) * (1 + P.MVAAJUSTADO/100)) 
                     ELSE 0 END)
             FROM TGFPRO P
                 LEFT JOIN TGFIPI I ON I.CODIPI = P.CODIPI
                 LEFT JOIN TGFICM ICM ON ICM.IDALIQ = T.IDALIQICMS 
+                LEFT JOIN TGFPEM PEM ON PEM.CODPROD = P.CODPROD AND PEM.CODEMP = 2
             WHERE P.CODPROD = T.CODPROD
         ),
         T.VLRSUBST = (
             SELECT 
                 (CASE WHEN ICM.ALIQSUBTRIB > 0 THEN 
                     (((T.VLRVENDA + (CASE WHEN P.TEMIPIVENDA = 'S' THEN 
-                        (T.VLRVENDA * I.PERCENTUAL / 100) ELSE 0 END)) * (1 + ICM.MARGLUCRO/100) * ICM.ALIQSUBTRIB/100) - 
+                        (T.VLRVENDA * I.PERCENTUAL / 100) ELSE 0 END)) * (1 + P.MVAAJUSTADO/100) * ICM.ALIQSUBTRIB/100) - 
                             (T.VLRVENDA * ICM.ALIQUOTA / 100)) ELSE 0 END)
             FROM TGFPRO P
                 LEFT JOIN TGFIPI I ON I.CODIPI = P.CODIPI
                 LEFT JOIN TGFICM ICM ON ICM.IDALIQ = T.IDALIQICMS 
+                LEFT JOIN TGFPEM PEM ON PEM.CODPROD = P.CODPROD AND PEM.CODEMP = 2
             WHERE P.CODPROD = T.CODPROD        
         ),
         T.VLRBASE = (
             SELECT 
-                (PR.VLRVENDA/(1+(((CASE WHEN ICM.ALIQSUBTRIB > 0 THEN (((T.VLRVENDA + 
-                    (CASE WHEN P.TEMIPIVENDA = 'S' THEN (T.VLRVENDA * I.PERCENTUAL / 100) ELSE 0 END)) * (1+ICM.MARGLUCRO/100) * 
-                        ICM.ALIQSUBTRIB/100) - (T.VLRVENDA * ICM.ALIQUOTA / 100)) ELSE 0 END)/T.VLRVENDA) +
-                        ((CASE WHEN P.TEMIPIVENDA = 'S' THEN NVL(I.PERCENTUAL,0) ELSE 0 END)/100))))
+                (PR.VLRVENDA/(1+(
+                    ((CASE WHEN ICM.ALIQSUBTRIB > 0 THEN 
+                        (((T.VLRVENDA + (CASE WHEN P.TEMIPIVENDA = 'S' THEN 
+                            (T.VLRVENDA * I.PERCENTUAL / 100) ELSE 0 END)) * (1+P.MVAAJUSTADO/100) * ICM.ALIQSUBTRIB/100) - 
+                                (T.VLRVENDA * ICM.ALIQUOTA / 100)) ELSE 0 END)/T.VLRVENDA) +
+                    ((CASE WHEN P.TEMIPIVENDA = 'S' THEN NVL(I.PERCENTUAL,0) ELSE 0 END)/100))))
             FROM TGFPRO P
                 LEFT JOIN TGFIPI I ON I.CODIPI = P.CODIPI
                 LEFT JOIN TGFICM ICM ON ICM.IDALIQ = T.IDALIQICMS 
                 LEFT JOIN AD_TGFTABIPC PR ON PR.CODPROD = P.CODPROD
+                LEFT JOIN TGFPEM PEM ON PEM.CODPROD = P.CODPROD AND PEM.CODEMP = 2
             WHERE P.CODPROD = T.CODPROD        
         ),
-        T.MVA = (SELECT ICM.MARGLUCRO FROM TGFICM ICM WHERE ICM.IDALIQ = T.IDALIQICMS) 
-    WHERE T.CODPROD > 0;
-
-    /*ATUALIZAR A TABELA AD_TGFTABIPC COM OS VALORES CALCULADOS*/
-
-    UPDATE AD_TGFTABIPC T
-    SET
-        MVA = (SELECT MVA FROM AD_TGFTABBASE WHERE CODPROD = T.CODPROD),
-        STCALC = (SELECT VLRSUBST FROM AD_TGFTABBASE WHERE CODPROD = T.CODPROD),
-        VLRTAB = (SELECT VLRBASE FROM AD_TGFTABBASE WHERE CODPROD = T.CODPROD)
-    WHERE T.CODPROD > 0;
+        T.MVA = (SELECT P.MVAAJUSTADO FROM TGFPEM PEM WHERE PEM.CODPROD = T.CODPROD AND PEM.CODEMP = 2) 
+            WHERE T.CODPROD > 0;
 
     /*INSERIR UMA NOVA LINHA NA TABELA DE PREÇO 2*/
 
     INSERT INTO TGFTAB (NUTAB,CODTAB,DTVIGOR,DTALTER,CODTABORIG,UTILIZADECCUSTO)
-       VALUES ((SELECT MAX(NUTAB)+1 FROM TGFTAB),2,SYSDATE,SYSDATE,2,'S');
+       VALUES ((SELECT MAX(NUTAB)+1 FROM TGFTAB),2,SYSDATE-1,SYSDATE,2,'S');
 
     INSERT INTO TGFEXC (NUTAB,CODPROD,VLRVENDA,TIPO,CODLOCAL)
        SELECT 
